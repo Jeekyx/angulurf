a3836707b706bc65e786f328c47cbf966cbe0776
Yvan Tcatchenko <y.tcatchenko@gmail.com>
2015-04-14 23:54:43 +0200

    Add some basic endpoints

---
+++ api/routes/data.js
@@ -0,0 +1,46 @@
+var Global = require('../models/models.js').Global;
+var Item = require('../models/models.js').Item;
+var Champion = require('../models/models.js').Global;
+var Spell = require('../models/models.js').Global;
+
+var express = require('express');
+var routerRandom = express.Router();
+
+var global;
+var items;
+var champions;
+var spells;
+
+// Retrive all the static data and the global data of all the urf games
+Global.findOne({}, '-_id -__v',function (err, doc) {
+	global = doc;
+});
+
+Item.find({}, '-_id -__v', function (err, docs) {
+	items = docs;
+});
+
+Champion.find({}, '-_id -__v', function (err, docs) {
+	champions = docs;
+});
+
+SummonerSpell.find({}, '-_id -__v', function (err, docs) {
+	spells = docs;
+});
+
+function sendResponse(res) {
+
+}
+
+routerRandom.get('/', function(req, res, next) {
+	console.log('GET /random');
+	if (req.query.leagues) {
+		var leagues = req.query.leagues.split(',');
+		console.log(req.query);
+		res.send(items);
+	} else {
+		res.status(400).send('no leagues specified');
+	}
+});
+
+module.exports.Random = routerRandom;
